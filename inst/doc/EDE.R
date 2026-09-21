d4c914a7885c7fda3df5b56f0593a1c923e1e817
Rodrigo Fonseca Villa <rodrigo03.villa@gmail.com>
2026-09-21 10:10:37 +0000

    version 0.2.0

--- inst/doc/EDE.R
+++ inst/doc/EDE.R
@@ -4,30 +4,40 @@ knitr::opts_chunk$set(collapse = TRUE, comment = "#>", fig.width = 7, fig.height
 ## -----------------------------------------------------------------------------
 library(EDE)
 
-years <- c(1900, 1902, 1903, 1905, 1907, 1908, 1910, 1912, 1915, 1918,
-           1920, 1923, 1925, 1928, 1930, 1933, 1936)
-sightings <- c(4, 3, 5, 2, 3, 4, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1)
-
-sd <- sighting_data(data.frame(year = years, sightings = sightings))
-sd
+years <- c(1900, 1902, 1903, 1905, 1907, 1908, 1910, 1912,
+           1915, 1918, 1920, 1923, 1925, 1928, 1930, 1933, 1936)
+counts <- c(4, 3, 5, 2, 3, 4, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1)
+sd <- sighting_data(data.frame(year = years, sightings = counts))
 
 ## -----------------------------------------------------------------------------
-ole(sd)
 robson1964(sd)
 strauss1989(sd)
+ole(sd)
+ole(sd, k = 10)
+
+## -----------------------------------------------------------------------------
 solow1993(sd, test_year = 2000)
 solow1993b(sd, test_year = 2000)
 solow2005(sd, test_year = 2000)
+mcinerny2006(sd, test_year = 2000)
 burgman1995(sd, test_year = 2000)
+solow_roberts2003(sd, test_year = 2000)
+jaric2010(sd, test_year = 2000)
 
 ## -----------------------------------------------------------------------------
-curve_1993 <- solow1993(sd, test_year = 2000, data_out = TRUE)
-curve_2005 <- solow2005(sd, test_year = 2000, data_out = TRUE)
+curve <- jaric2010(sd, test_year = 2000, data_out = TRUE)
+plot(curve$time, curve$chance, type = "l",
+     xlab = "candidate time", ylab = "p-value")
+abline(h = 0.05, lty = 2)
 
-plot(curve_1993$time, curve_1993$chance, type = "l", lwd = 2, col = "steelblue",
-     ylim = c(0, 1), xlab = "candidate extinction year", ylab = "chance of persistence")
-lines(curve_2005$time, curve_2005$chance, lwd = 2, col = "firebrick")
-abline(h = 0.05, lty = 2, col = "grey50")
-legend("topright", legend = c("Solow (1993)", "Solow (2005)", "alpha = 0.05"),
-       col = c("steelblue", "firebrick", "grey50"), lty = c(1, 1, 2), lwd = c(2, 2, 1), bty = "n")
+## -----------------------------------------------------------------------------
+uncertain <- sighting_data(data.frame(
+  year = c(1900, 1910, 1920, 1930),
+  sightings = 1
+))
+
+jaric_roberts2014(
+  uncertain,
+  reliability = c(1.0, 0.9, 0.6, 0.3)
+)
 
